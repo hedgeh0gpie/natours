b9e95f2b0ba2fd59dacfc9c7d1ab0ae11c6f7da3
Sebastian McKelvey <sebastian.mckelvey@gmail.com>
2022-04-06 18:02:58 -0500

    Refactor for MVC

--- server.js
+++ server.js
@@ -19,23 +19,6 @@ mongoose.connect(DB).then(() => {
 //   console.log('Local DB connection successful!');
 // });
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
-const Tour = mongoose.model('Tour', tourSchema);
-
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
