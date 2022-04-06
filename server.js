a7ae92602acdbe592683978f1b796b6e0b3b72ae
Sebastian McKelvey <sebastian.mckelvey@gmail.com>
2022-04-06 16:27:11 -0500

    Add hosted and local DB connections with Mongoose

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
+
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+// HOSTED DATABASE
+mongoose.connect(DB).then(() => {
+  console.log('DB connection successful!');
+});
+
+// LOCAL DATABASE
+// mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
+//   console.log('Local DB connection successful!');
+// });
 
 const port = process.env.PORT || 3000;
 
