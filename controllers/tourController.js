fb122e239bae698a793a1fe2e28b4388028351bc
Sebastian McKelvey <sebastian.mckelvey@gmail.com>
2022-04-07 12:24:03 -0500

    Implement field limiting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -23,6 +23,14 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    // Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v'); // Exclude '__v' field that MongoDB uses when returning response to client
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 
